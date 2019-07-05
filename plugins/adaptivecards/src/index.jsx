5549a6c8f5844470c64b6da0e28b7bead4bec0be
Robin Schuster <r.schuster@cognigy.com>
2019-07-05 17:50:42 +0200

    clean up css styles and performance

--- plugins/adaptivecards/src/index.jsx
+++ plugins/adaptivecards/src/index.jsx
@@ -1,51 +1,27 @@
 import * as React from 'react';
-import memoize from 'memoize-one';
 import AdaptiveCard from 'react-adaptivecards';
 
-import { CSSInjectionStyles } from './styles';
-
-// only re-calculate if theme changed
-const getStylesMemo = memoize(CSSInjectionStyles);
+import { updateAdaptiveCardCSSCheaply } from './styles';
 
 // map to remember which message had already been resolved
 const resolvedCognigyMessages = new Map();
 
-// set style tag to customize adaptive cards display
-const head = document.head || document.getElementsByTagName('head')[0],
-    style = document.createElement('style');
-
-head.appendChild(style);
-style.type = 'text/css';
-style.id = 'accss';
-
 class AdaptiveCards extends React.Component {
     render() {
-        const message = this.props.message;
-        const payload = message && message.data && message.data._plugin && message.data._plugin.payload;
-        const customWidth = message && message.data && message.data._plugin && message.data._plugin.width;
-        const customBorder = message && message.data && message.data._plugin && message.data._plugin.border;
-
         const {
             theme,
-            onSendMessage
+            onSendMessage,
+            message
         } = this.props;
 
-        let css = document.getElementById('accss');
-        const {
-            acPushbuttonSubdued,
-            acPushbuttonExpanded,
-            acInput
-        } = getStylesMemo(theme);
+        updateAdaptiveCardCSSCheaply(theme);
 
-        css.innerHTML = `
-            .ac-pushbutton ${acPushbuttonSubdued};    
-            .ac-pushbutton.subdued ${acPushbuttonSubdued};
-            .ac-pushbutton.style-default.expandable.expanded ${acPushbuttonExpanded};
-            .ac-input ${acInput};
-        `;
+        const {
+            payload
+        } = message.data._plugin;
 
         const onActionSubmit = (params) => {
-            onSendMessage("", { "adaptivecards": params && params.data});
+            onSendMessage("", { "adaptivecards": params && params.data });
         }
 
         const hostConfig = {
@@ -53,13 +29,19 @@ class AdaptiveCards extends React.Component {
         }
 
         if (resolvedCognigyMessages && !resolvedCognigyMessages.has(message.traceId)) {
-            resolvedCognigyMessages.set(message.traceId, <AdaptiveCard style={{boxShadow: theme.shadow, width: customWidth || '450px', border: customBorder || '1px solid #cccccc'}} payload={payload} onActionSubmit={onActionSubmit} hostConfig={hostConfig}/>);
+            resolvedCognigyMessages.set(message.traceId, (
+                <AdaptiveCard
+                    payload={payload}
+                    onActionSubmit={onActionSubmit}
+                    hostConfig={hostConfig}
+                />
+            ));
         }
-        
+
         return (
-            <div style={{ width: '95%', paddingLeft: '10px'}}>
+            <div className='adaptivecard-wrapper'>
                 {resolvedCognigyMessages.get(message.traceId)}
-            </div>       
+            </div>
         )
     }
 }
