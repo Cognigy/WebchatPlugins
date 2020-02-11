e8600eaefcfa71a2b07678cc3e0cbabffed286c6
Alexander Teusz <teusz.alexander@gmail.com>
2020-02-11 17:33:50 +0100

    working plugin

--- plugins/push-notification/src/index.jsx
+++ plugins/push-notification/src/index.jsx
@@ -6,13 +6,47 @@ const PushNotification = (props) => {
     const { message } = props;
     const { text, data } = message;
     const { _plugin } = data;
-    const { type, title, round } = _plugin;
+    const { variant, title, round  } = _plugin;
+
+    // Set background color for push notification
+    let background;
+    switch (variant) {
+        case "success": 
+            background = "green";
+            break;
+        case "warning":
+            background = "orange";
+            break;
+        case "error":
+            background = "red";
+            break;
+    }
 
     return (
         <div style={{
-            display: "flex"
+            display: "flex",
+            flexDirection: "column",
+            padding: "5%",
+            color: "white",
+            background: background,
+            borderRadius: round ? "25px" : 0,
+            marginLeft: round ? "2%" : 0,
+            marginRight: round ? "2%" : 0,
+
         }}>
-            
+            <div style={{
+                // insert custom styles for title here ...
+                fontSize: "150%",
+                fontWeight: "bold",
+                marginBottom: "5%"
+            }}>
+                {title}
+            </div>
+            <div style={{
+               // insert custom styles for text here ... 
+            }}>
+                {text}
+            </div>
         </div>
     )
 
