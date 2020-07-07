f95d461dd205e0320a15e53faf1278d5bd2127f7
Alexander Teusz <teusz.alexander@gmail.com>
2020-07-07 10:58:11 +0200

    removed unsused box import

--- plugins/rating/src/index.jsx
+++ plugins/rating/src/index.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Rating from '@material-ui/lab/Rating';
-import { Box, Button, Typography } from '@material-ui/core/';
+import { Button, Typography } from '@material-ui/core/';
 
 const UserRating = (props) => {
 
