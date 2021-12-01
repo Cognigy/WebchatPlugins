92dbe790bb9531138ec6f4a7f1986c065651ad92
Christian Buchwald <schinkenpapst@googlemail.com>
2021-12-01 15:02:50 +0100

    bug/19538 modify call parameters for secure authentication

--- plugins/file-upload/src/helpers/upload.js
+++ plugins/file-upload/src/helpers/upload.js
@@ -29,15 +29,14 @@ export const upload = async (config, file) => {
                 .then(() => downloadUrl)
         }
         case 'live-agent': {
-            const { userApiKey, host, accountId, conversationId } = config;
-            const uploadUrl = `${host}/api/v1/accounts/${accountId}/conversations/${conversationId}/messages`;
+            const { inboxIdentifier, host, conversationId, contactIdentifier } = config;
+            const uploadUrl = `${host}/public/api/v1/inboxes/${inboxIdentifier}/contacts/${contactIdentifier}/conversations/${conversationId}/messages`;
             const form = new FormData();
             form.append('attachments[]', file);
             form.append('content', 'file-upload');
 
             return Axios.post(uploadUrl, form, {
                 headers: {
-                    'api_access_token': userApiKey,
                     'Content-Type': 'multipart/form-data',
                     'Accept': '*/*',
                 },
