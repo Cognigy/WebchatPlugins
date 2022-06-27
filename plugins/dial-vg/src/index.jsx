5d3c8fb4c498315469b37fe8be141f245db6680a
Alex Teusz <a.teusz@cognigy.com>
2022-06-27 09:23:30 +0200

    wip

--- plugins/dial-vg/src/index.jsx
+++ plugins/dial-vg/src/index.jsx
@@ -7,163 +7,144 @@ import { UserAgent, Inviter } from "sip.js";
 const DialVG = (props) => {
 
 	// get info from Cogngiy data
-	const { message, isFullscreen, onSetFullscreen, attributes } = props;
+	const { message, attributes } = props;
 	const { data } = message;
 	const { _plugin } = data;
 	const { sip } = _plugin;
-	const { aor, username, password, server } = sip;
+	const { aor, username, password, server, target } = sip;
 
-	const [callStatus, setCallStatus] = React.useState('');
+	const [callStatus, setCallStatus] = React.useState('Calling');
 	const [sipSession, setSipSession] = React.useState();
 
-	if (!isFullscreen) {
-		return (
+	React.useEffect(() => {
+		// SIP Addresses-of-Record URI associated with the user agent.
+		const uri = UserAgent.makeURI(aor);
+
+		const userAgentOptions = {
+			uri,
+			logLevel: "log",
+			authorizationPassword: password,
+			authorizationUsername: username,
+			transportOptions: {
+				server: server
+			}
+		};
+
+		const userAgent = new UserAgent(userAgentOptions);
+
+		userAgent.start().then(async () => {
+			const targetUri = UserAgent.makeURI(target);
+
+			const inviter = new Inviter(userAgent, targetUri);
+			inviter.invite({
+				sessionDescriptionHandlerOptions: {
+					constraints: {
+						audio: true,
+						video: false
+					}
+				}
+			}).then((session) => {
+				setCallStatus("Cognigy Support");
+				setSipSession(session);
+			});
+		});
+
+	}, []);
+
+	return (
+		<div
+			{...attributes}
+			style={{
+				...attributes.styles,
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'space-between'
+			}}>
 			<div style={{
-				textAlign: 'center',
+				display: 'flex',
+				flex: 1,
+				justifyContent: 'center',
+				alignItems: 'center'
+			}}>
+				<span
+					style={{
+						fontSize: '200%'
+					}}
+				>
+					{callStatus}
+				</span>
+			</div>
+			<div style={{
+				flex: 3,
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center'
+			}}>
+				<div style={{
+					display: 'flex',
+					justifyContent: 'center',
+					flexWrap: 'wrap',
+					width: '60%'
+				}}>
+					{
+						['1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '0', '#'].map(dtmfOption => (
+							<button
+								style={{ cursor: 'pointer', margin: '3%', height: '50px', width: '50px', border: '1px solid grey', padding: '15px', borderRadius: '50px', background: 'transparent' }}
+								onClick={() => {
+									if (sipSession !== undefined) {
+										sipSession.info({
+											requestOptions: {
+												body: {
+													contentDisposition: "render",
+													contentType: "application/dtmf-relay",
+													content: `Signal=${dtmfOption}\r\nDuration=1000`
+												}
+											}
+										});
+									}
+
+								}}
+								disabled={!sipSession}
+							>
+								{dtmfOption}
+							</button>
+						))
+					}
+				</div>
+			</div>
+			<div style={{
+				display: 'flex',
+				flex: 1,
+				alignItems: 'center',
+				justifyContent: 'center'
 			}}>
 				<button
-					type='button'
 					style={{
 						padding: '15px',
 						borderRadius: '50px',
 						border: 'none',
-						backgroundColor: 'green',
+						backgroundColor: 'red',
 						color: 'white',
 						height: '50px',
 						width: '50px',
 						cursor: 'pointer'
 					}}
-					onClick={async () => {
-
-						// SIP Addresses-of-Record URI associated with the user agent.
-						const uri = UserAgent.makeURI(aor);
-
-						const userAgentOptions = {
-							uri,
-							logLevel: "log",
-							authorizationPassword: password,
-							authorizationUsername: username,
-							transportOptions: {
-								server: server
-							}
-						};
-
-						const userAgent = new UserAgent(userAgentOptions);
-
-						onSetFullscreen();
-
-						await userAgent.start();
-						const target = UserAgent.makeURI("sip:test@cognigy-alexteusz.sip.jambonz.us");
-
-						setCallStatus("Calling");
-						const inviter = new Inviter(userAgent, target);
-						const session = await inviter.invite({
-							sessionDescriptionHandlerOptions: {
-								constraints: {
-									audio: true,
-									video: false
-								}
-							}
-						});
-
-						setCallStatus("Cognigy Support");
-						setSipSession(session);
-					}}
-				></button>
-				<audio autoplay></audio>
+					onClick={() => {
+						sipSession.reject();
+						sipSession.bye();
+						onDismissFullscreen();
+					}}></button>
 			</div>
-		);
-	} else {
-
-		return (
-			<div
-				{...attributes}
-				style={{
-					...attributes.styles,
-					display: 'flex',
-					flexDirection: 'column',
-					justifyContent: 'space-between'
-				}}>
-				<div style={{
-					display: 'flex',
-					flex: 1,
-					justifyContent: 'center',
-					alignItems: 'center'
-				}}>
-					<span
-						style={{
-							fontSize: '200%'
-						}}
-					>
-						{
-							callStatus
-						}
-					</span>
-				</div>
-				<div style={{
-					flex: 3,
-					display: 'flex',
-					justifyContent: 'center',
-					alignItems: 'center'
-				}}>
-					<div style={{
-						display: 'flex',
-						justifyContent: 'center',
-						flexWrap: 'wrap',
-						width: '60%'
-					}}>
-						{
-							['1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '0', '#'].map(dtmfOption => (
-								<button
-									style={{ cursor: 'pointer', margin: '3%', height: '50px', width: '50px', border: '1px solid grey', padding: '15px', borderRadius: '50px', background: 'transparent' }}
-									onClick={() => {
-										if (sipSession !== undefined) {
-											sipSession.dtmf(dtmfOption);
-										}
-
-									}}
-									disabled={!sipSession}
-								>
-									{dtmfOption}
-								</button>
-							))
-						}
-					</div>
-				</div>
-				<div style={{
-					display: 'flex',
-					flex: 1,
-					alignItems: 'center',
-					justifyContent: 'center'
-				}}>
-					<button
-						style={{
-							padding: '15px',
-							borderRadius: '50px',
-							border: 'none',
-							backgroundColor: 'red',
-							color: 'white',
-							height: '50px',
-							width: '50px',
-							cursor: 'pointer'
-						}}
-						onClick={() => {
-							sipSession.reject();
-							sipSession.bye();
-							onDismissFullscreen();
-						}}></button>
-				</div>
-				<audio autoplay></audio>
-			</div>
-		);
-	}
+			<audio id="audio" autoplay></audio>
+		</div>
+	);
 }
 
 const dialVGPlugin = {
 	match: 'dial-vg',
 	component: DialVG,
 	options: {
-		fullwidth: true
+		fullscreen: true
 	}
 }
 
