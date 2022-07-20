c565df94c182b5bd5b92433a7f970321d79a9809
Edi Bibergall <e.bibergall@cognigy.com>
2022-07-20 12:00:55 +0200

    minor changes still in development

--- plugins/uipath-attended-robot/src/index.jsx
+++ plugins/uipath-attended-robot/src/index.jsx
@@ -1,56 +1,57 @@
 import * as React from 'react';
 import { UiPathRobot } from '@uipath/robot';
+import memoize from 'memoize-one'
+import { getStyles } from '../../file-upload/src/styles';
 
+const getStylesMemo = memoize(getStyles);
 const processedMessages = new Set();
 
+const UiPathAttendedRobots = (props) => {
 
-const UiPathAttendedRobots = async (props) => {
-
-  const { onSendMessage, message } = props;
-  
-    const robot = UiPathRobot.init();
-    
-    const processes = await robot.getProcesses()
-    const sample = processes[0]
-    console.log(sample)
+  const {
+    onSendMessage,
+    message
+  } = props;
 
   // Get local unattended robot processes
-  /*const getLocalProc = () => {
-  
-    
-    
+  const getLocalProc = React.useCallback(async processList => {
 
-    UiPathRobot.getProcesses().then(processList => {
-      onSendMessage("it worked", {
-        processes: processList
-      });
-    }, err => {
-      onSendMessage("error", {
-        processesError: err
-      })
-    })
-    
+    processList = await UiPathRobot.getProcesses()
+
+    if (processList.length === 0) {
+      console.error("Robot not connected or no processes available")
+    }
+
+    let sample = processList[0].name
+
+
+    /*onSendMessage("it worked", {
+      processes: processList
+    });*/
+
+    return (
+      <button
+        type='button'
+        onClick={() => onSendMessage('hi')}
+      >
+        {sample}
+      </button>
+    );
   }
+  )
 
-
-  // Only execute the plugin once
-  /*if (!processedMessages.has(message.traceId)) {
+  /*// Only execute the plugin once
+  if (!processedMessages.has(message.traceId)) {
     if (message.data?._plugin?.type === 'attended-robots') {
       processedMessages.add(message.traceId);
       getLocalProc();
     }
-  }*/
+  }
+*/
 
-  return (
-    (
-      <button
-          type='button'
-          onClick={() => onSendMessage('hi')}
-      >
-      send 'hi'
-      </button>
-  )
-  );
+
+  return null;
+
 }
 
 const uiPathAttendedRobotsPlugin = {
